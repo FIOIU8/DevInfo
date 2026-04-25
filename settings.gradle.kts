c1ee8944d19e0abde48a388daaeb9952ae52d684
FIOIU8 <lava0bucket@163.com>
2026-04-25 18:08:33 +0800

    fix: 移除已废弃的 kotlin-android 插件，使用 AGP 9.0 内置 Kotlin 支持

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://jitpack.io")
     }
 }
 
